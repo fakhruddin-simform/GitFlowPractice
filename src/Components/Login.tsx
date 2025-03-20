46ae4f734230244b4e00913917ac81d4c930e7e8
Fakhruddin Bhabrana <fakhruddin@simformsolutions.com>
2025-03-20 20:11:58 +0530

    Add login screen

---
+++ src/Components/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <input type="text" placeholder='First Name' />
+      <input type="text" placeholder='Last Name' />
+    </div>
+  )
+}
+
+export default Login
